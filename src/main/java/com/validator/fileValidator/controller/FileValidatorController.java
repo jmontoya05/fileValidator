317b1e8d76af6d1cccf0a7e5b336605f3ed13ff9
jmontoya05 <santiagomontoya997@gmail.com>
2023-10-09 14:45:10 -0500

    Se definen los atributos del controlador como final

--- src/main/java/com/validator/fileValidator/controller/FileValidatorController.java
+++ src/main/java/com/validator/fileValidator/controller/FileValidatorController.java
@@ -14,8 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1")
 public class FileValidatorController {
 
-    private CSVValidatorService csvValidatorService;
-    private XLSXValidatorService xlsxValidatorService;
+    private final CSVValidatorService csvValidatorService;
+    private final XLSXValidatorService xlsxValidatorService;
     @Autowired
     public FileValidatorController(CSVValidatorService csvValidatorService, XLSXValidatorService xlsxValidatorService) {
         this.csvValidatorService = csvValidatorService;
@@ -24,7 +24,7 @@ public class FileValidatorController {
 
     @PostMapping("/csv")
     public boolean validateCSV(@RequestBody Person person) {
-        return this.csvValidatorService.validateCSV(person);
+            return this.csvValidatorService.validateCSV(person);
     }
 
     @PostMapping("/xlsx")
